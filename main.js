046cf3b1fd7e4474e04f60272b8738fe6b67feb1
Aidana-Asylbek-kyzy <aidanaasylbekkyzy12@gmail.com>
2023-05-28 22:01:30 +0600

    дополнительное не поняла

---
+++ main.js
@@ -0,0 +1,27 @@
+// let i = 0;
+// function printArrayValues(){
+//     i++;
+//     console.log(i, ' array ');
+//     if(i === 3){
+//         return;
+//     }else{
+//         return printArrayValues();
+//     }
+// }
+
+// printArrayValues();
+
+///////////////////////////////
+
+// const str = 'Myname457isAlex';
+// const regex = /\D/g;
+// const result = str.match(regex);
+// console.log(result);
+
+///////////////////////////////
+
+// const str = 'Hello\World\nTest';
+// const regex = /\S/gmi;
+// const result = str.match(regex);
+// console.log(result);
+
